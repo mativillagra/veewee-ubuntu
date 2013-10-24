ae5a5576e61b5fd64c54eaabb790de260d9d23dd
Southwest Airlines <developer@laptop-devops.(none)>
2013-10-24 09:15:58 -0300

    Initial project

---
+++ manifests/default.pp
@@ -0,0 +1,19 @@
+exec { "apt-get update":
+ 	command => "/usr/bin/apt-get update",
+}
+
+package { "apache2":
+	require => Exec["apt-get update"],
+}
+
+
+package { "vim":
+        ensure => present,
+}
+
+file { "/var/www":
+	ensure => link,
+	target => "/vagrant",
+	force => true,
+}
+
